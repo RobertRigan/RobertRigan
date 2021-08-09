fa7069d81be1f295578900f40efd061216c78887
RobertRigan <88643510+RobertRigan@users.noreply.github.com>
2021-08-09 06:15:21 +0200

    Update and rename README.md to aboutMe.php

---
+++ aboutMe.php
@@ -0,0 +1,6 @@
+- 👋 Hi, I’m @RobertRigan
+- 👀 I’m interested in Web development
+- 🌱 I’m currently learning Git and React
+- 💞️ I’m looking to collaborate on Website project
+- 📫 U can reach je on www.robertrigan.sk
+
